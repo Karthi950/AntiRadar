582787022e9e7881f7b72510b444894a5f4952e7
Karthi <karthikan293@gmail.com>
2016-05-04 18:48:10 +0200

    add Paris location and Button on layout

--- app/src/main/java/com/example/karthi/antiradar/MapsActivity.java
+++ app/src/main/java/com/example/karthi/antiradar/MapsActivity.java
@@ -38,9 +38,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // Add a marker in Paris and move the camera
+        LatLng paris = new LatLng(48,2);
+        mMap.addMarker(new MarkerOptions().position(paris).title("Marker in Paris"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(paris));
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 5.0f ) );
     }
 }
