3130046c89f4a4cd14fa4656dad5ea40e58e4ec1
Karthi <karthikan293@gmail.com>
2016-07-03 10:55:14 +0200

    add activity for settings

--- app/src/main/java/com/example/karthi/antiradar/MapsActivity.java
+++ app/src/main/java/com/example/karthi/antiradar/MapsActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -87,6 +88,8 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            setContentView(R.layout.setting_layout);
+            Log.d("MENU Setting","yo je suis dans le menu setting");
             return true;
         }
 
